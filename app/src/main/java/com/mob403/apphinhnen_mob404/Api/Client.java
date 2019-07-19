bdbfbf1467fcb30027121cb4970900ae8895da44
GiaGioi <nguyengiagioi99@gmail.com>
2019-07-19 22:39:34 +0700

    Gioidz commit

---
+++ app/src/main/java/com/mob403/apphinhnen_mob404/Api/Client.java
@@ -0,0 +1,4 @@
+package com.mob403.apphinhnen_mob404.Api;
+
+public class Client {
+}
--- app/src/main/java/com/mob403/apphinhnen_mob404/UI/SplashActivity.java
+++ app/src/main/java/com/mob403/apphinhnen_mob404/UI/SplashActivity.java
@@ -2,7 +2,9 @@ package com.mob403.apphinhnen_mob404.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.mob403.apphinhnen_mob404.R;
 
@@ -14,6 +16,14 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        new Handler().postDelayed (new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent (SplashActivity.this, MainActivity.class);
+                startActivity(i);
 
+                finish ();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
